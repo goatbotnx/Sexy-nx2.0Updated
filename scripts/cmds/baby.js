54fb7a55818d2f90fd0e037e9e9880496e10c0cf
goatbotnx <onlygoatbot.nx@gmail.com>
2025-12-05 01:03:03 +0600

    Update baby.js

--- scripts/cmds/baby.js
+++ scripts/cmds/baby.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const mahmud = [
+const xalman = [
   "baby",
   "bby",
   "jan",
@@ -9,17 +9,18 @@ const mahmud = [
   "xadika"
 ];
 
-
 const baseApiUrl = async () => {
-  const base = await axios.get("https://raw.githubusercontent.com/mahmudx7/exe/main/baseApiUrl.json");
+  const base = await axios.get(
+    "https://raw.githubusercontent.com/goatbotnx/nx-goat-bot/refs/heads/main/API.json"
+  );
   return base.data.jan;
 };
 
 module.exports = {
   config: {
     name: "bot",
-    version: "1.7",
-    author: "MahMUD",
+    version: "1.8",
+    author: "mahmud || modified by xalman",
     role: 0,
     category: "ai",
     guide: { en: "just type jan" },
@@ -34,7 +35,9 @@ module.exports = {
       async function getBotResponse(message) {
         try {
           const base = await baseApiUrl();
-          const response = await axios.get(`${base}/jan/font2/${encodeURIComponent(message)}`);
+          const response = await axios.get(
+            `${base}/jan/font2/${encodeURIComponent(message)}`
+          );
           return response.data?.message;
         } catch {
           return "error janu🥹";
@@ -59,30 +62,34 @@ module.exports = {
   onChat: async function ({ api, event }) {
     const responses = [
       "kmn acho bby🥺",
-      "assalamualaikum bbz🫠💋",
-      "eto din por mine porlo amake🥺💔",
-      "hea bolo 🥳🥳",
-      "kire haramjada dakoch kn 😠",
-      "kache adho bby🌚🫶",
-      "ummahh bbz🌚🫶",
-      "kisse ko 🫤",
-      "nx er bow nai or bow hoba ...?🤧🌚",
-      "__ফ্রী ফে'সবুক চালাই কা'রন ছেলেদের মুখ দেখা হারাম 😌",
-      "মন সুন্দর বানাও মুখের জন্য তো 'Snapchat' আছেই! 🌚"
+      "assalamualaikum bbz🫠",
+      "eto din por mone porlo amake🥺",
+      "hea bolo 🥳",
+      "kire dakoch kn 😐",
+      "i love you 😙 ❤️ ",
+      "fake you bby 🤣 🌚",
+      "kache asho 🙂",
+      "umm… ki hoyeche?",
+      "hmm bolo?",
+      "__kmn acho jan🙂",
+      "ami sure Tor keo nai tai amare dakli😌"
     ];
 
     const message = event.body?.toLowerCase() || "";
     const words = message.split(" ");
     const wordCount = words.length;
 
-    if (event.type !== "message_reply" && mahmud.some(word => message.startsWith(word))) {
+    if (event.type !== "message_reply" && xalman.some(word => message.startsWith(word))) {
+
       api.setMessageReaction("🪶", event.messageID, () => {}, true);
       api.sendTypingIndicator(event.threadID, true);
 
-      async function getBotResponse(message) {
+      async function getBotResponse(text) {
         try {
           const base = await baseApiUrl();
-          const response = await axios.get(`${base}/jan/font3/${encodeURIComponent(message)}`);
+          const response = await axios.get(
+            `${base}/jan/font2/${encodeURIComponent(text)}`
+          );
           return response.data?.message;
         } catch {
           return "error janu🥹";
@@ -98,13 +105,14 @@ module.exports = {
               type: "reply",
               messageID: info.messageID,
               author: event.senderID,
-              link: randomMsg,
+              text: randomMsg,
             });
           }
         }, event.messageID);
       } else {
         const userText = words.slice(1).join(" ");
         const botResponse = await getBotResponse(userText);
+
         api.sendMessage(botResponse, event.threadID, (err, info) => {
           if (!err) {
             global.GoatBot.onReply.set(info.messageID, {
